b0fa68d216aeb8097e1f8b825c3dd908e4429a63
Sunil Dagur <sunildagur566@gmail.com>
2023-12-06 17:52:23 +0530

    Add project files.

---
+++ Assignmnent 5_5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class ExceptionHandlingDemo
+{
+    static void Main()
+    {
+        try
+        {
+           
+            int[] numbers = { 1, 2, 3 };
+            int indexOutOfRangeValue = numbers[5];
+            
+            string invalidNumberString = "abc";
+            int parsedNumber = int.Parse(invalidNumberString); 
+           
+            throw new InvalidOperationException("This is an example of InvalidOperationException.");
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine($"IndexOutOfRangeException caught: {ex.Message}");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"FormatException caught: {ex.Message}");
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"InvalidOperationException caught: {ex.Message}");
+        }
+        catch (Exception ex)
+        {
+            
+            Console.WriteLine($"Unexpected exception caught: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("This block always executes, regardless of whether an exception occurred or not.");
+        }
+
+        Console.WriteLine("Program continues execution after the try-catch-finally block.");
+    }
+}
